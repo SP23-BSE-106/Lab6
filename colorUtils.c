02360c49a10415fb9fc892ee9efb00a8d8d9095a
Munazza Javed <munazzajaved277@gmail.com>
2023-12-22 00:40:03 +0500

    Error toGrayScale

--- colorUtils.c
+++ colorUtils.c
@@ -11,9 +11,11 @@ int min(int a, int b, int c) {
   return (a < b) ? (a < c ? a : c) : (b < c ? b : c);
 }
 
-int toGrayScale(int *r, int *g, int *b, Mode mode) {
+Error toGrayScale(int *r, int *g, int *b, Mode mode) {
   //TODO: implement
-  
+  if(r == NULL || g == NULL || b == NULL ){
+    return ERROR_PRESENT;
+  }
 }
 
 int toSepia(int *r, int *g, int *b) {
