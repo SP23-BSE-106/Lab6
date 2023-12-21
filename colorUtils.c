b395a450c622e1f72542b155fed08ae6170689c6
Munazza Javed <munazzajaved277@gmail.com>
2023-12-22 00:42:05 +0500

    Average,lightness and luminosity technique

--- colorUtils.c
+++ colorUtils.c
@@ -16,6 +16,18 @@ Error toGrayScale(int *r, int *g, int *b, Mode mode) {
   if(r == NULL || g == NULL || b == NULL ){
     return ERROR_PRESENT;
   }
+   if (mode == AVERAGE) {
+    *r = *g = *b = (*r + *g + *b) / 3;
+  }
+  //FOR LIGHTNESS TECHNIQUE
+  else if (mode == LIGHTNESS) {
+    *r = *g = *b = (max(*r, *g, *b) + min(*r, *g, *b))/2;
+  } 
+  //FOR LUMINOSITY TECHNIQUE
+  else if (mode == LUMINOSITY) {
+    *r = *g = *b = round(0.21 * *r + 0.72 * *g + 0.07 * *b);
+  }
+  return NO_ERROR;
 }
 
 int toSepia(int *r, int *g, int *b) {
